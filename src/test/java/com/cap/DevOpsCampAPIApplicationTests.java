570c7ce3be44f32c79e6cfd7e1d2949b397a295a
Harvey1327 <harvey_bal@hotmail.co.uk>
2017-04-03 10:54:40 +0100

    Fixed Junit

--- src/test/java/com/cap/DevOpsCampAPIApplicationTests.java
+++ src/test/java/com/cap/DevOpsCampAPIApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SimpleWebServiceApplicationTests {
+public class DevOpsCampAPIApplicationTests {
 
 	@Test
 	public void contextLoads() {
