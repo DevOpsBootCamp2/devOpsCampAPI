a2bdf7a17835e5829c007290550568fc33f3e475
Harvey1327 <harvey_bal@hotmail.co.uk>
2017-04-03 14:05:20 +0100

    Fixed JUnit

--- src/test/java/com/cap/domain/UserTest.java
+++ src/test/java/com/cap/domain/UserTest.java
@@ -3,7 +3,10 @@ package com.cap.domain;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 public class UserTest {
 
 	@Test
