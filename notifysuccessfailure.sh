0327fe2b523e33fc3204e0c59dd08a3ca2e14f84
mgopaul <maya.gopaul@capgemini.com>
2017-04-04 16:06:52 +0100

    drone and amend notify

--- notifysuccessfailure.sh
+++ notifysuccessfailure.sh
@@ -1,1 +1,1 @@
-curl -X POST -H 'Content-type: application/json'  --data '{"text":"The Boot Camp Application is now available.\n Any issues please contact the Boot Camp Team on 911."}'  https://hooks.slack.com/services/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
+ 'Content-type: application/json'  --data '{"text":"The Boot Camp Application is now available.\n Any issues please contact the Boot Camp Team on 911."}'  https://hooks.slack.com/services/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
