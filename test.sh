bfc15f3b0946021112d99e9253c0c59c3f605d97
Arukha <swarnalata.arukha@capgemini.com>
2017-04-06 12:14:27 +0100

    chaged test and docker file

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 /usr/bin/java -jar /DevOpsCampAPI-0.0.1-SNAPSHOT.jar &
 ID=$! # ID of webserver process, so we can kill it
 
 tests_passed=true
-expected="{\"status\":\"P\"}"
+expected="{\"status\":\"UP\"}"
 output=$(curl -s localhost:8090/health)
 echo "$output"
 if [[ "$expected" != "$output" ]]; then
